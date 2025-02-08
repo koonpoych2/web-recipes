7dc9f60328b69dde4c0e7d3d0008611c9761fc7c
koonpoych2 <poywayuphak@gmail.com>
2025-02-08 12:43:57 +0700

    set-up getParameters from aws

--- back-end/database/db.go
+++ back-end/database/db.go
@@ -1,9 +1,13 @@
 package database
 
 import (
+	"context"
 	"fmt"
 	"log"
-	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -11,9 +15,58 @@ import (
 
 var DB *gorm.DB
 
+func getParameters(names []string) (map[string]string, error) {
+
+	// Load AWS configuration
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		return nil, fmt.Errorf("failed to load AWS config: %w", err)
+	}
+
+	// Create SSM client
+	client := ssm.NewFromConfig(cfg)
+
+	// Fetch parameters
+	resp, err := client.GetParameters(context.TODO(), &ssm.GetParametersInput{
+		Names:          names,
+		WithDecryption: aws.Bool(true),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get parameters: %w", err)
+	}
+
+	// Store parameters in a map
+	params := make(map[string]string)
+	for _, param := range resp.Parameters {
+		params[*param.Name] = *param.Value
+	}
+
+	return params, nil
+}
+
 func ConnectDB() {
-	var err error
-	dsn := os.Getenv("DATABASE_ENDPOINT")
+
+	parameterNames := []string{
+		"/mycook/db-host",
+		"/mycook/db-name",
+		"/mycook/db-password",
+		"/mycook/db-username",
+	}
+
+	params, err := getParameters(parameterNames)
+	if err != nil {
+		log.Fatal("Error retrieving parameters:", err)
+	}
+
+	fmt.Println("Retrieved parameters:", params)
+	// var err error
+
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		params["/mycook/db-username"], params["/mycook/db-password"],
+		params["/mycook/db-host"], "3306", params["/mycook/db-name"],
+	)
+
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
--- back-end/go.mod
+++ back-end/go.mod
@@ -3,6 +3,9 @@ module github.com/koonpoych2/web-recipes/back-end
 go 1.22.2
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.36.1
+	github.com/aws/aws-sdk-go-v2/config v1.29.6
+	github.com/aws/aws-sdk-go-v2/service/ssm v1.56.12
 	github.com/gofiber/fiber/v2 v2.52.6
 	github.com/joho/godotenv v1.5.1
 	gorm.io/driver/mysql v1.5.7
@@ -13,6 +16,17 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.59 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.28 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.32 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.32 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.13 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.24.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.28.14 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.33.14 // indirect
+	github.com/aws/smithy-go v1.22.2 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
